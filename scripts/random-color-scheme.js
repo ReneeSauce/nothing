24c41d401a2edeed21ff30afa7b5e3d12184db8e
ReneeSauce <ReneeSauce@users.noreply.github.com>
2023-01-30 19:17:41 -0800

    create random-color-scheme.js

---
+++ scripts/random-color-scheme.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto')
+
+
+
+const color = crypto.randomBytes(3).toString('hex')
+const backgroundColor = crypto.randomBytes(3).toString('hex')
+const blobColor = crypto.randomBytes(3).toString('hex')
+const linkColor = crypto.randomBytes(3).toString('hex')
+
+document.getElement
+
+export function randomColorScheme() {
+	
+}
